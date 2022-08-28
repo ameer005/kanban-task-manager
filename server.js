9d3d68fabf88632ac475592646e9445ca611a4b0
Ameer <ak2260650@gmail.com>
2022-08-28 13:36:36 +0530

    fixed paths

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.json());
 // });
 
 if (process.env.NODE_ENV === "production") {
-  app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
