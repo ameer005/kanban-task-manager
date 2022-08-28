27b825adc0bc89730aeb1eacc6b1b4c0086ec023
Ameer <ak2260650@gmail.com>
2022-08-28 12:51:56 +0530

    fixed vercel paths

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client", "build")));
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
